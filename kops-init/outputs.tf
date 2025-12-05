151852502b111a742c173a6041b1021ad3f18d44
Global Mobarak <mh.ice.iu@gmail.com>
2025-12-05 16:02:06 +0600

    formated outputs

--- kops-init/outputs.tf
+++ kops-init/outputs.tf
@@ -1,7 +1,7 @@
 output "kops_init" {
   description = "Kops initialization infrastructure outputs"
   value = {
-    cluster_name      = var.cluster_name
-    state_bucket      = module.kops_state_store.bucket_name
+    cluster_name = var.cluster_name
+    state_bucket = module.kops_state_store.bucket_name
   }
 }
