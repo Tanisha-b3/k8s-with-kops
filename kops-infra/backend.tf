f7ce767e56402223b98092018bfa999a2eb71815
Tanisha borana <101504198+Tanisha-b3@users.noreply.github.com>
2025-12-10 14:27:05 +0530

    Delete kops-infra/backend.tf

--- kops-infra/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    # Configure via backend config file or CLI
-    # bucket = "your-kops-state-bucket"
-    # key    = "infra/kops-terraform.tfstate"
-    # region = "your-region"
-  }
-}
